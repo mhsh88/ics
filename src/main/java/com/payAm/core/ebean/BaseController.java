12e4001469bb0176455ea95524f2b47863c365a0
hossein sharifi <hosseinsharifi@hotmail.com>
2018-04-18 01:13:19 +0000

    copy code from play restcontroller to spring basecontroller

--- src/main/java/com/payAm/core/ebean/BaseController.java
+++ src/main/java/com/payAm/core/ebean/BaseController.java
@@ -1,43 +1,31 @@
 package com.payAm.core.ebean;
 
-import com.fasterxml.jackson.core.JsonFactory;
-import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.base.Throwables;
-import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import com.payAm.core.dto.PageDto;
 import com.payAm.core.i18n.CoreMessagesCodes;
-import com.payAm.core.model.BaseEntity;
+import com.payAm.core.util.ReflectionUtil;
+import com.payAm.core.view.BaseView;
 import org.apache.commons.beanutils.BeanUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.web.JsonPath;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.ui.Model;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.core.Context;
-import javax.ws.rs.core.Request;
-import javax.ws.rs.core.UriInfo;
-import javax.xml.ws.Response;
 import java.io.IOException;
 import java.io.Serializable;
-import java.lang.reflect.Field;
-import java.lang.reflect.Modifier;
 import java.lang.reflect.ParameterizedType;
 import java.util.*;
+import play.libs.Json;
 
 
-public abstract class BaseController<T, ID extends Serializable> {
+
+public abstract class BaseController<T, ID extends Serializable, V extends BaseView> {
 
     private Logger logger = LoggerFactory.getLogger(RestController.class);
 
@@ -47,9 +35,11 @@ public abstract class BaseController<T, ID extends Serializable> {
     public BaseController(CrudRepository<T, ID> repo) {
         this.repo = repo;
     }
+    protected void afterLoad(T model) { }
 
     @RequestMapping/*(value = "/{queryString}", method=RequestMethod.GET)*/
-    public @ResponseBody ResponseEntity<T> listAll(@RequestParam Map<String,String> params
+    public @ResponseBody
+    ResponseEntity<String> listAll(@RequestParam Map<String,String> params
                                                  ) throws IOException {
         PageDto pageDto = getPageDtoFromJson(params);
 
@@ -132,15 +122,30 @@ public abstract class BaseController<T, ID extends Serializable> {
 //        }
 //        PageDto page6 = mapper.convertValue(actualObj, PageDto.class);
 
-        PageResult<T> pageResult = new PageResult<>();
+        PageResult<T> modelsPageResult = new PageResult<>();
 
-
-        Iterable<T> all = this.repo.findAll();
-        for(T a : all){
-            pageResult.addData(a);
+        try {
+            List<String> fields = getFetchedFields();
+            pageDto.setFetchFields(fields);
+            Iterable<T> all = this.repo.findAll();
+            for(T a : all){
+                modelsPageResult.addData(a);
+            }
+            modelsPageResult = find(pageDto);
+            for (T model : modelsPageResult.getData()) {
+                afterLoad(model);
+            }
+            return ResponseEntity.ok().body(writeJson(modelsPageResult));
+        }
+        catch (Exception e) {
+            modelsPageResult.unsuccessfulOperation(e.getMessage());
+            return ResponseEntity.badRequest().body(writeJson(modelsPageResult));
         }
 
-        pageResult.setMessage(CoreMessagesCodes.SUCCESSFUL_LOAD_MODEL);
+
+
+
+//        modelsPageResult.setMessage(CoreMessagesCodes.SUCCESSFUL_LOAD_MODEL);
 
 
 //        response.setContentType("text/plain");
@@ -148,7 +153,7 @@ public abstract class BaseController<T, ID extends Serializable> {
 
 
 
-        return (ResponseEntity<T>) ResponseEntity.ok().body(pageResult);
+//        return (ResponseEntity<T>) ResponseEntity.ok().body(writeJson(modelsPageResult));
 //        return Lists.newArrayList(all);
     }
 
@@ -216,50 +221,49 @@ public abstract class BaseController<T, ID extends Serializable> {
         return m;
     }
 
-//    @RequestMapping(method=RequestMethod.POST, consumes={MediaType.APPLICATION_JSON_VALUE})
-//    public @ResponseBody Map<String, Object> update( @RequestBody T json/*, */) {
-//
-////        @PathVariable ID json.id;
-//
-//        System.out.println(json);
-//        System.out.println(json.getClass());
-////        Integer id = null;
-//        logger.debug("update() of id#{} with body {}", /*id,*/ json);
-//        logger.debug("T json is of type {}", json.getClass());
-//
-//        ID id = (ID) "1";
-//
-//
-//        T entity = this.repo.findOne(id);
-//
-//
-//
-//        try {
-//            BeanUtils.copyProperties(entity, json);
-//        }
-//        catch (Exception e) {
-//            logger.warn("while copying properties", e);
-//            throw Throwables.propagate(e);
-//        }
-//
-//        logger.debug("merged entity: {}", entity);
-//
-//        T updated = this.repo.save(entity);
-//        logger.debug("updated enitity: {}", updated);
-//
-//        Map<String, Object> m = Maps.newHashMap();
-//        m.put("success", true);
-//        m.put("id", id);
-//        m.put("updated", updated);
-//        return m;
-//    }
-
     @RequestMapping(value="/{id}", method=RequestMethod.DELETE)
     public @ResponseBody Map<String, Object> delete(@PathVariable ID id) {
         this.repo.delete(id);
         Map<String, Object> m = Maps.newHashMap();
         m.put("success", true);
         return m;
+    }
+
+    protected List<String> getFetchedFields() {
+        return ReflectionUtil.getAllEntityColumnNames(getModelClass());
+    }
+    protected Class<T> getModelClass() {
+        Class<?> clazz = getClass();
+        while (clazz.getGenericSuperclass() != null) {
+            if (clazz.getGenericSuperclass() instanceof ParameterizedType) {
+                return (Class<T>) ((ParameterizedType) clazz.getGenericSuperclass()).getActualTypeArguments()[0];
+            } else {
+                clazz = clazz.getSuperclass();
+            }
+        }
+        return null;
+    }
+    protected PageResult<T> find(PageDto page) throws Exception {
+        return getDao().find(page);
+    }
+    protected Class<V> getViewClass() {
+        Class<?> clazz = getClass();
+        while (clazz.getGenericSuperclass() != null) {
+            if (clazz.getGenericSuperclass() instanceof ParameterizedType) {
+                return (Class<V>) ((ParameterizedType) clazz.getGenericSuperclass()).getActualTypeArguments()[2];
+            } else {
+                clazz = clazz.getSuperclass();
+            }
+        }
+        return null;
+    }
+
+    protected String writeJson(PageResult<T> pageResult) throws JsonProcessingException {
+        return Json.mapper().writerWithView(getViewClass()).writeValueAsString(pageResult);
+    }
+
+    protected T readJson(JsonNode jsonNode) throws java.io.IOException {
+        return Json.mapper().readerWithView(getViewClass()).forType(getModelClass()).readValue(jsonNode);
     }
 }
 
